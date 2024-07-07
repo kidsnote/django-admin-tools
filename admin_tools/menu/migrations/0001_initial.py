e7927596e22de3da1a586c6ceb65dab63a112dd5
snoya <snoya00@gmail.com>
2024-07-07 20:55:00 +0900

    커밋 누락된 마이그레이션 추가

--- admin_tools/menu/migrations/0001_initial.py
+++ admin_tools/menu/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('url', models.CharField(max_length=255)),
                 ('title', models.CharField(max_length=255)),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_constraint=False)),
             ],
             options={
                 'ordering': ('id',),
